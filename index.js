5f0c215253f9a9e5a93ed0f116c0b35d4a73475e
aditay0102 <aditya0102.be21@chitkara.edu.in>
2023-09-06 22:42:16 -0700

    basic dom-manipulation

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+let bd = document.querySelector("body");
+
+let p = document.createElement("p");
+p.textContent = "hey i am red"
+p.setAttribute('style', 'color:red;')
+bd.appendChild(p); 
 
 
-///////////////// ============== ;;;;;;;;;;;;;;;; llllllllll           OBJECT cONSTRUCTOR
+let h3 = document.createElement("h3");
+h3.textContent = "I'am blue h3!"
+h3.setAttribute('style','color:blue;')
 
+bd.appendChild(h3)
 
-let  n = parseInt(prompt("enter your number"));
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+h1.textContent = "I am a div"
+div.appendChild(h1)
 
+let dp = document.createElement("p");
+dp.textContent = "Me too";
 
-for(let i = 0;i<n;i++){
-    if( i%3 == 0){
-        console.log("fizz")
-    }
-    else if(i%5 == 0){
-        console.log("buzz")
-    }
-    else{
-        console.log(i)
-    }
+div.appendChild(dp)
 
-}
+bd.appendChild(div)
